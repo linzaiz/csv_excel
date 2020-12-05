808262f27a37c439ebe6d587f68682dd112ad2b9
Linzaiz <linzaiz@hotmail.com>
2020-12-05 15:55:52 +0800

    # v1.02     2020-12-5 900   使用当前路径下的Templates文件夹，找不到的话再用绝对路径。使用pathlib

--- Zpayment1/Zpayment--GitHub-Save!!!--follow-III.py
+++ Zpayment1/Zpayment--GitHub-Save!!!--follow-III.py
@@ -3,20 +3,20 @@
 # 程序性质：Excel xls/xlsx 处理
 # 功能：从付款信息大表读入，按要求分别存到付款文件中
 #
-# 使用方法： 1. 在工作目录建立Templates子目录，先填好里面模板
-#           2. 更新工作目录 working_path
-#           3. python运行
-# 输出： 全部输出到工作目录里
-# 注意事项： 1. 设置 working_path， 如 working_path = r'\财务\付款申请——2020-11--III'
-#           2. 设置Excel读取记录数 num_of_row
-#           3. 以及Excel列标题从第几行开始等
+# 使用方法： 1. 在工作文件夹建立Templates子文件夹，先填好里面模板
+#           2. 运行此.py程序
+# 输出： 全部输出到与Templates并列的output文件夹里
+# 注意事项： 1. 注意：设置 各文件夹路径、模板文件名、列名与本程序匹配。不改变的话可直接运行。'
+#           2. 也要注意：Excel标题行在第几行，等等。
 # *****************************************************************
-# auth__ = 'Larry Zhang' # 2020-11-24 18:08
+# auth__ = 'Larry Zhang' # 2020-11-24 1808
 # v1.00     2020-11-24      不同的乙方，这版全，但日期都留空了（因合同没签完）
 # v1.01     2020-12-5       乙方相同的才放在一起。乙方名称、银行放到信息大表里了。
+# v1.02     2020-12-5 900   使用当前路径下的Templates文件夹，找不到的话再用绝对路径。使用pathlib
 
-# import sys
+import sys
 import os
+from pathlib import Path, PureWindowsPath
 import pandas as pd
 # import re
 # import datetime, time
@@ -47,8 +47,17 @@ def chn_date(np_datetime):
         return ''
 
 
-def a_contract_prt(outtable, i):
-    wb = load_workbook('templates\\' + outtable)  # encoding='gb18030'不像csv模块，没有这个encoding。
+def a_contract_prt(outtable_str, i, output_path):
+    try:
+        wb = load_workbook(templ_path / outtable_str)  # encoding='gb18030'不像csv模块，没有这个encoding。
+    except Exception as e:
+        print(f"出现错误！！！文件【{templ_path / outtable_str}】: \n【{repr(e)}】！")  # e.message str(e)
+        while input("回车退出...") != '':
+            pass
+        else:
+            print("程序已出错并结束。")
+        os._exit(-9)  # sys.exit(-9)
+
     wb.guess_types = True  # 加个这个，写入百分数后就会显示百分数，否则显示小数
     # print( 'wb.get_named_ranges()=', wb.get_named_ranges() )
     sheetNames = wb.sheetnames
@@ -58,7 +67,7 @@ def a_contract_prt(outtable, i):
     print( sh.title, sh.max_row, sh.max_column )
     print( get_column_letter(sh.max_column) )  # 从1开始
 
-    if '付款申请单' in outtable:
+    if '付款申请单' in outtable_str:
         sh["B6"].value = pay_to_co
         sh["B7"].value = dfBig['合同名称'][i].replace('\n', '')
         sh["B8"].value = chn_date(dfBig['合同签订时间'][i])
@@ -78,7 +87,7 @@ def a_contract_prt(outtable, i):
             pass
         sh["B13"].value = stmp
 
-    elif '付款台账' in outtable:
+    elif '付款台账' in outtable_str:
         sh["B2"].value = pay_to_co
         sh["B4"].value = dfBig['付款单位'][i]
         sh["C4"].value = dfBig['合同金额\n（元）'][i]
@@ -88,7 +97,7 @@ def a_contract_prt(outtable, i):
         sh["G4"].value = dfBig['应付金额\n（元）'][i]
         sh["H4"].value = chn_date(dfBig['付款时间'][i])
         sh["I4"].value = dfBig['备注'][i]
-    elif '付款审批单' in outtable:
+    elif '付款审批单' in outtable_str:
         sh["B6"].value = pay_to_co
         sh["B10"].value = dfBig['项目'][i].replace('\n', '')
         sh["B14"].value = dfBig['合同名称'][i].replace('\n', '')
@@ -100,28 +109,42 @@ def a_contract_prt(outtable, i):
         sh["B19"].value = f"    根据我单位与{pay_to_co}签订的《" + dfBig['合同名称'][i].replace('\n', '') + "》的约定，申请支付" \
                           + str(dfBig['应付金额\n（元）'][i]) + "元。\n    妥否，请领导批示。"
 
-    newf1 = os.path.splitext(outtable)[0] + dfBig['文件名缩写'][i] + '.xlsx'
-    newf1 = str(i) + newf1[1:]
-    wb.save(filename=os.path.join(working_path, newf1))
+    newfn = os.path.splitext(outtable_str)[0] + dfBig['文件名缩写'][i] + '.xlsx'
+    newfn = str(i) + newfn[1:]  # 替换模板文件名第一个字母，比如 ‘x6’ 换为 16
+    wb.save(filename=output_path / newfn)
     wb.close()
 
 
 if __name__ == '__main__':
-    working_path = r'.\in_out_example'  # <----改1.文件夹全路径
-    templ_path = os.path.join(working_path, "Templates")
-    iptFP = os.path.join(templ_path, r'zzz、付款信息汇总大表.xlsx')
+    main_path0, _ = os.path.split(os.path.abspath(sys.argv[0]))  # 本主程序所在路径
+    main_path = Path(main_path0)  # pathlib化
+
+    templ_path = main_path / 'templates'
+    if not templ_path.exists():
+        tmpP = PureWindowsPath(r'C:\tmp\付款申请——2020-12——IV\templates')  # <----不用本程序所在文件夹下的templates的话，用这个路径!!！！
+        templ_path = Path(tmpP)
+    else:
+        pass
+    if not templ_path.exists():
+        print(f"出现错误！！！程序所在文件夹下没找到模板文件夹：【{main_path / 'templates'}】，临时模板路径【{templ_path}】也不存在！")
+        while input("回车退出...") != '':
+            pass
+        else:
+            print("程序已出错并结束。")
+        os._exit(-9)  # sys.exit(-9)
+
+    out_path = templ_path / '../output'
+    if not out_path.exists():
+            os.mkdir(out_path)
+    iptFP = templ_path / 'zzz、付款信息汇总大表.xlsx'
     # 没用了：num_of_row = 5
 
-    outt1 = 'x1、付款申请单 - .xlsx'
-    outt5 = 'x5、付款台账 - .xlsx'
-    outt6 = 'x6、付款审批单 - .xlsx'
+    # template names for output, they are strings:
+    outts1 = 'x1、付款申请单 - .xlsx'
+    outts5 = 'x5、付款台账 - .xlsx'
+    outts6 = 'x6、付款审批单 - .xlsx'
 
-    outt_hz = 'z7、票据汇总单 - .xlsx'
-
-    outt1 = os.path.join(working_path, outt1)
-    outt5 = os.path.join(working_path, outt5)
-    outt6 = os.path.join(working_path, outt6)
-    outt_hz = os.path.join(working_path, outt_hz)
+    outts_hz = 'z7、票据汇总单 - .xlsx'
 
     if not os.access( iptFP, os.F_OK ):
         print( "访问不了源表(付款信息大表)【%s】 ! please check..." % iptFP )
@@ -130,9 +153,18 @@ if __name__ == '__main__':
 
     # ##############读取大表#########################
     print( 'Loading Mapping File【', iptFP, '】，Please wait ...... ' )
-    mapX = pd.ExcelFile( iptFP )
-    dfBig = pd.read_excel( mapX, sheet_name=u'大表', header=3 - 1, usecols="A: Q", index_col=u'序号' )  # header默认=0，是第0+1行，=2是指2+1行。
-    dfTitle = pd.read_excel( mapX, sheet_name=u'大表', header=None, skiprows=0, nrows=2, dtype=str, usecols="A: I", index_col=None)
+    try:
+        mapX = pd.ExcelFile( iptFP )
+        dfBig = pd.read_excel( mapX, sheet_name=u'大表', header=3 - 1, usecols="A: Q", index_col=u'序号' )  # header默认=0，是第0+1行，=2是指2+1行。
+        dfTitle = pd.read_excel( mapX, sheet_name=u'大表', header=None, skiprows=0, nrows=2, dtype=str, usecols="A: I", index_col=None)
+    except Exception as e:
+        print(f"出现错误！！！文件【{iptFP}】: \n【{repr(e)}】！")
+        while input("回车退出...") != '':
+            pass
+        else:
+            print("程序已出错并结束。")
+        os._exit(-9)  # sys.exit(-9)
+
     pay_to_co = dfTitle.iloc[1, 1]
     bank_name = dfTitle.iloc[1, 3]
     bank_acc = dfTitle.iloc[1, 5]
@@ -143,7 +175,16 @@ if __name__ == '__main__':
     # os.chdir( working_path )
 
     # 票据汇总单
-    wbhz = load_workbook('.\\templates\\' + outt_hz)  # encoding='gb18030'不像csv模块，没有这个encoding。
+    try:
+        wbhz = load_workbook(templ_path / outts_hz)  # encoding='gb18030'不像csv模块，没有这个encoding。
+    except Exception as e:
+        print(f"出现错误！！！文件【{templ_path / outts_hz}】: \n【{repr(e)}】！")
+        while input("回车退出...") != '':
+            pass
+        else:
+            print("程序已出错并结束。")
+        os._exit(-9)  # sys.exit(-9)
+
     wbhz.guess_types = True  # 加个这个，写入百分数后就会显示百分数，否则显示小数
     SNs = dfBig.index.dropna().astype(int)
     for i in SNs:   # range(1, num_of_row + 1):
@@ -156,11 +197,11 @@ if __name__ == '__main__':
         print( get_column_letter(sh.max_column) )  # 从1开始
         sh[f"A{7 + i}"].value = dfBig['项目'][i].replace('\n', '')
         sh[f"F{7 + i}"].value = dfBig['应付金额\n（元）'][i]
-    wbhz.save(filename=os.path.join(working_path, outt_hz))
+    wbhz.save(filename=out_path / outts_hz)
     wbhz.close()
 
     # 按每个合同输出Excel文件：
     for i in SNs:    # range(1, num_of_row + 1):   # 直接改数吧 range1-13 =12个。 len(dfBig.index)
-        a_contract_prt(outt1, i)
-        a_contract_prt(outt5, i)
-        a_contract_prt(outt6, i)
+        a_contract_prt(outts1, i, out_path)
+        a_contract_prt(outts5, i, out_path)
+        a_contract_prt(outts6, i, out_path)
